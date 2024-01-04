efa3e5690bcb7670a07e2506c32ac5cc5af1b8ff
eag <92303346+ElenaAyora@users.noreply.github.com>
2024-01-04 19:52:05 +0100

    Add files via upload

--- Personaje.hs
+++ Personaje.hs
@@ -3,18 +3,26 @@ module Personaje
     crearPersonaje,
     obtenerPosicion,
     obtenerPuntosVida,
-    obtenerObjetos
+    obtenerObjetos,
+    obtenerPuntosAtaque
     ) where
+
+-- importo la librería Random, para general un valor aleatorio para los puntos de ataque del personaje
+import System.Random
 
 data Personaje = Personaje
     { posicion :: (Int, Int),
-    puntosVida :: Int,
-    objetos :: [String]
-    }
+      puntosVida :: Int,
+      objetos :: [String],
+      puntosAtaque :: Int
+    } deriving (Show)
+
 
 -- Función para crear al personaje
+-- (en principio los puntos de ataque van a ser estáticos, pero lo ideal sería que fuera aleatorio)
 crearPersonaje :: Personaje
-crearPersonaje = Personaje { posicion = (0, 0), puntosVida = 100, objetos = [] }
+crearPersonaje = Personaje { posicion = (0, 0), puntosVida = 100, objetos = [], puntosAtaque = 10 }
+
 
 -- FUNCIONES INFORMACIÓN DEL PERSONAJE
 -- Posición en la que está (sala de la mazmorra)
@@ -28,3 +36,7 @@ obtenerPuntosVida = puntosVida
 -- Objetos que ha ido recolectando el personaje en las diferentes salas
 obtenerObjetos :: Personaje -> [String]
 obtenerObjetos = objetos
+
+-- Puntos de ataques del personaje
+obtenerPuntosAtaque :: Personaje -> Int
+obtenerPuntosAtaque = puntosAtaque
