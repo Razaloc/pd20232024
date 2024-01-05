80972114c42036bc865ba779f33fa22fd5915112
Rafael Garcia Ruiz <llerrak@hotmail.com>
2024-01-05 02:38:00 +0100

    Secuencia de turnos
    Se comienza a implementar el inicio de partida y la secuencia de turnos
    
    Signed-off-by: Rafael Garcia Ruiz <llerrak@hotmail.com>

--- Matrices.hs
+++ Matrices.hs
@@ -1,12 +1,11 @@
 module Matrices
-    ( Vector, 
-    Matriz, 
-    numColumnas, 
-    separa, 
-    matrizLista,
-    primeraPosicion,
-    ultimaPosicion
-    ) where
+( Vector
+, Matriz
+, numColumnas
+, separa
+, matrizLista
+, listaMatriz
+) where
 
 
 import Data.Array
@@ -25,16 +24,17 @@ type Vector a = Array Int a
 -- naturales. 
 type Matriz a = Array (Int,Int) a
 
--- Esto debería ir en el modulo de matriz
 numColumnas :: (Num a) => Matriz a -> Int
 numColumnas = snd . snd . bounds
 
--- Esto debería ir en el modulo de matriz
 separa :: Int -> [a] -> [[a]]
 separa _ [] = []
 separa n xss = (take n xss):separa n (drop n xss)
 
--- Esto debería ir en el modulo de matriz
 matrizLista :: (Num a) => Matriz a -> [[a]]
 matrizLista p = (separa (numColumnas p) (elems p))
 
+listaMatriz :: Num a => [[a]] -> Matriz a
+listaMatriz xss = listArray ((1,1), (f, c)) (concat xss)
+   where f = length xss
+         c = (length.head) xss 
--- main.hs
+++ main.hs
@@ -12,9 +12,22 @@ import System.IO
 import Matrices
 
 -- ---------------------------------------------------------------------
+-- El tipo mazmorra representa el estado actual de la partida, es una matriz de ceros y unos que representan si una sala está resuelta o no.
 type Mazmorra = Matriz Int
--- data player 
+-- Tipo posición, coordenadas dentro de la mazmorra.
+type Pos = (Int, Int)
 
+type Objeto = String
+-- data player 
+data Aventurero = Aventurero 
+  {nombre::String
+  ,posicion::Pos
+  ,vida::Int 
+  ,nivel::Float 
+  ,mochila::[Objeto]} deriving (Show)
+
+nuevoAventurero :: Aventurero
+nuevoAventurero = Aventurero {nombre = "Alex", posicion = (1,1), vida = 100, nivel = 0, mochila = []}
 
 -- Recibe un numero y crea una mazmorra no resuelta de ese tamaño, indices de (1,1) a (n,n)
 nuevaMazmorra :: Int -> Mazmorra
@@ -23,12 +36,27 @@ nuevaMazmorra n = listArray ((1,1), (f, c)) (concat xss)
          c = (length.head) xss 
          xss = replicate n . replicate n $ 1
 
--- Imprime el estado actual de la mazmorra
+-- Imprime aventurero
+imprimeAventurero :: Aventurero -> IO ()
+imprimeAventurero a = putStrLn (show a) 
+
+-- Aventurero en formato texto para guardado
+textoAventurero :: Aventurero -> String
+textoAventurero a = (nombre a) ++ "\n" ++ (show (posicion a)) ++ (show (vida a)) ++ "\n" ++ (show (nivel a)) ++ "\n" ++ (show (mochila a) )
+
+
+-- Imprime el estado actual de la mazmorra en pantalla
 imprimeMazmorra :: Mazmorra -> IO ()
 imprimeMazmorra arr = putStr (unlines [unwords [show (arr ! (x, y)) | x <- [1..n]] | y <- [1..n]])
     where n = (numColumnas arr)
 
+-- Mazmorra en formato texto para guardado
+textoMazmorra :: Mazmorra -> String
+textoMazmorra m =  show (matrizLista m)
 
+-- Texto de la info esencial de la partida
+textoPartida :: (Mazmorra, Aventurero) -> String
+textoPartida (m,a) = (textoMazmorra m) ++ "/n" ++ (textoAventurero a)
 
 
 -- Comprueba condición de victoria
@@ -36,13 +64,17 @@ finalizado :: Mazmorra -> Bool
 finalizado m =  m ! (x,x) == 0
     where x = numColumnas m
 
--- Resuelve sala, cambia el valor x,y de la matriz a 0
-resuelveSala :: (Int, Int) -> Mazmorra -> Mazmorra
+-- Resuelve sala, cambia el valor (x, y) de la matriz a 0
+resuelveSala :: Pos -> Mazmorra -> Mazmorra
 resuelveSala (x,y) ar = ar // [((x,y), 0)]
 
+--Consulta si la sala ha sido resuelta
+esResueltaSala :: Pos -> Mazmorra -> Bool
+esResueltaSala (x,y) m =  m ! (x,y) == 0
 
-
-
+-- Cambia el valor x,y de la matriz. De momento sin utilidad. 
+cambiaValorSala :: Pos -> Mazmorra -> Int -> Mazmorra
+cambiaValorSala (x,y) ar a = ar // [((x,y), a)]
 
 
 --separa2 :: String -> [Int]
@@ -61,9 +93,8 @@ leeDigito c = do
             leeDigito c
 
 -- (por implementar) lee partida guardada
-leerPartida :: IO (Int, [Int])
-leerPartida = undefined
---leerPartida = do
+--cargarPartida ::  IO (Mazmorra, Aventurero)
+--cargarPartida = do
 --    putChar '\n'
 --    putStrLn "Indique el nombre del archivo: "
 --    f <- getLine -- para leer una línea entera (coge una cadena de caracteres)
@@ -71,13 +102,22 @@ leerPartida = undefined
 --    if existe then do
 --        contenido <- readFile f
 --        let lineas  = lines contenido
---        let jugador = read (head lineas)
---        let tablero = [ read x | x <- separa2 (last lineas)] -- separa2 (last lineas)
---        return (jugador, tablero)
+--        let aventurero = read (head lineas)
+--        let heroe = Aventurero {nombre = nombre , vida = vida, nivel = nivel, mochila = mochila}
+--        return (mazmorra, heroe)
 --        else do putStrLn "El archivo indicado no existe, intételo de nuevo"
---                leerPartida
+--                cargarPartida
 
 
+guardaPartida :: (Mazmorra, Aventurero) -> IO ()
+guardaPartida (m, a) = writeFile "guarda.txt" $ textoPartida (m, a)
+
+
+actualizaAventurero :: Aventurero -> Pos -> Aventurero
+actualizaAventurero a (x,y) =  a {posicion = (x,y)}  
+
+
+suma (x, y) (u, v) = (x+u, y+v)
 
 -- Nuevo juego (por implementar)
 nuevoJuego :: IO ()
@@ -87,22 +127,84 @@ nuevoJuego = do
     putStrLn "2. Cargar partida de archivo"
     o <- leeDigito "Elija una opción: "
     if o == 2 then do
---        (j,t) <- leerPartida
+--        (maz,avent) <- cargarPartida
         putChar '\n'
---        juego t j
+--        juego maz avent
         else do 
             putChar '\n'
-              --n <- leeDigito "Elija una opción: "
-              -- let  = (nuevaMazmorra n)
-              -- let player = playerInicial
---            turno mazmorra player
+            n <- leeDigito "Elije un tamaño de la mazmorra que quieres visitar: "
+            juego (nuevaMazmorra n) nuevoAventurero
 
-turno :: Mazmorra -> Int -> IO ()
-turno m p = juego m p 
+--imposiblePosicion :: Pos -> Mazmorra -> Bool
+--imposiblePosicion (x, y) m
+--    | x <= 0 = False
+--    | x > z = False 
+--    | y <= 0 = False 
+--    | y > z = False 
+--    | otherwise = True
+--    where z = numColumnas m
 
-juego :: Mazmorra -> Int -> IO ()
-juego = undefined
---juego t j = do
+--regulate ::  IO Int ->  Int 
+--regulate n = case n of
+--    1 -> 1
+--    2 -> 2
+--    3 -> 3
+--    4 -> 4
+--    otherwise  -> 0
+--        
+
+-- Indica el movimiento deseado, ante un input no valido el movimiento será (0,0)
+--movimiento :: Int -> Pos
+--movimiento o = 
+--    case o of
+--        1 -> (1,0)
+--        2 -> (1,0)
+--        3 -> (1,0)
+--        4 -> (1,0)
+--        otherwise  -> (0,0)
+
+--preguntaMovimiento ::  IO Int
+--preguntaMovimiento = do
+--    putStrLn "Indica hacia donde quieres moverte: " 
+--    putStrLn "1. Norte"
+--    putStrLn "2. Sur"
+--    putStrLn "3. Este"
+--    putStrLn "4. Oeste"
+--    o <- leeDigito "Elija una opción: "
+--    return o
+
+juego :: Mazmorra -> Aventurero -> IO ()
+juego mazmorra aventurero = do
+    imprimeAventurero aventurero
+    imprimeMazmorra mazmorra
+    -- Comienza un turno normal
+    -- Display contenido de sala
+    -- Resuelve la sala
+    let postMazmorra = resuelveSala (posicion aventurero) mazmorra
+    -- Comprueba la condición de victoria
+    if not (finalizado mazmorra) then do
+        putStrLn "Elige en que dirección deseas moverte: "
+
+-- movimiento recibe aventurero y deuelve aventurero al que hay que comprobar si la posición es válida.
+--        let movimienDeseado = movimiento (regulate (preguntaMovimiento))
+--        let postAventurero = actualizaAventurero aventurero movimienDeseado 
+--        validar posicion 
+--        if imposiblePosicion (posicion postAventurero) mazmorra then do
+--            putStrLn "No puedes moverte en esa dirección, intentalo de nuevo"
+--            juego postMazmorra aventurero
+--            else 
+--                juego postMazmorra postAventurero
+        else
+            putStrLn "Enhorabuena has ganado"
+--    if (esResueltaSala posicion mazmorra) then do
+--        let actualizadoAventurero = movimiento aventurero
+--        putStrLn "Esta sala ya ha sido resuelta, ¿hacia donde deseas ir?"
+--        else do
+--            resuelveSala posicion mazmorra
+--            putStrLn "¡Explorando una nueva sala encontraste algo!"
+
+
+
 --    escribeTablero t
 --    if finalizado t
 --    then do putStrLn $ "J " ++ (show (siguiente j)) ++ "ha ganado"
