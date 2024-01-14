4882ad5a2a3a6fcd1e041b52e88e1175e3bfe42c
Rafael Garcia Ruiz <llerrak@hotmail.com>
2024-01-14 20:30:28 +0100

    update
    Signed-off-by: Rafael Garcia Ruiz <llerrak@hotmail.com>

--- Juego.hs
+++ Juego.hs
@@ -28,7 +28,6 @@ guardaPartida :: (Mazmorra, Aventurero) -> IO ()
 guardaPartida (m, a) = writeFile "guarda.txt" $ textoPartida (m, a)
 
 
-
 -- El sistema de batalla resume y simula una batalla por turnos en la cual cada uno hace el daño de su arma o propio al otro hasta que muere el enemigo. 
 lucha :: Aventurero -> Monstruo -> Aventurero
 lucha av monst = nuevoAventurero' (obtenerNombreAventurero av) (obtenerPosicionAventurero av) (sufre (obtenerVidaAventurero av) (obtenerMochilaAventurero av) monst) (obtenerMochilaAventurero av)
@@ -39,46 +38,52 @@ sufre pV (obj, valor) monst = pV - ((obtenerPuntosAtaqueMonstruo monst) * (divis
 division :: Int -> Monstruo -> Int
 division valor monst = (obtenerPuntosVidaMonstruo monst) `div` valor
 
+
+-- nos dice el número de salas resueltas al terminar la partida
 salasResueltas :: Partida -> Int
 salasResueltas p@(m,a) = sum [ 1 | x <- (concat(matrizLista m)), x == 'O']
 
+-- Este mensaje saltará cuando temrines la partida, informandote del total de salas que has resuelto
 titulo :: Int -> String
 titulo n = case n of 
     0 -> "¿¿Cero salas??"
     1 -> "Solo has resuelto una sala, puedes hacerlo mejor."
     2 -> "Has resuelto dos salas, estás por buen camino."
-    3 -> "Tres salas... Ni las tortugas van tan lentas, date brio"
-    4 -> "Genial, ya empiezas a ir por buen camino, cuatro salas"
-    5 -> "Cinco salas... Un cinco suele ser un aprobado, pero esto no se aplica si eres un héroe"
-    6 -> "Seis salas, estas progresando adecuadamente, sigue así"
-    7 -> "Se nota que eres un buen estratega, sigue así, siete salas"
-    8 -> "Ocho salas resueltas, nada más"
-    9 -> "Dale amigo/a, estas ya a nada de ser considerado un aventurero de rango E, nueve salas resueltas"
-    10 -> "Diez salas resueltas. Ojala yo en mis examenes, eres un buen héroe"
+    3 -> "Tres salas... Ni las tortugas van tan lentas, date brio."
+    4 -> "Genial, ya empiezas a ir por buen camino, cuatro salas."
+    5 -> "Cinco salas... Un cinco suele ser un aprobado, pero esto no se aplica si eres un héroe."
+    6 -> "Seis salas, estas progresando adecuadamente, sigue así."
+    7 -> "Se nota que eres un buen estratega, sigue así, siete salas."
+    8 -> "Ocho salas resueltas, nada más."
+    9 -> "Dale amigo/a, estas ya a nada de ser considerado un aventurero de rango E, nueve salas resueltas."
+    10 -> "Diez salas resueltas. Ojala yo en mis examenes, eres un buen héroe."
     11 -> "Por lo menos sabemos que no moriras por culpa de un slime, once salas resueltas."
     _ -> "¡¡ Eres increible has resuelto más de once salas !!"
-    
+
+-- Este mensaje saltará cuando temrines la partida, dandote un título segun el arma con el que terminasla partida
 tituloArma :: Objeto -> String
-tituloArma (_ ,1) = "¿Todavías estas con el palo de la fregona? ... cobarde ..."
-tituloArma (_ ,2) = "Conseguiste palo de babas al matar el slime, seras conocido como el señor de los mocos"
-tituloArma (_ ,5) = "Terminaste la partida con daga envenenada, ni tan mal, pero hay cosas mejores"
-tituloArma (_ ,8) = "Ganaste con la espada de hueso, relagalasela a tu enamorada, seguro que le encanta (jaja no, por favor no)"
-tituloArma (_ ,10) = "Terminaste la partida con el baston Descomunal, ya sabes, usalo para volver a casa"
-tituloArma (_ ,13) = "Ganaste la partida con las garras de lobo, apartir de ahora eres El gran hombre lobo pecho peludo que no usa tutú"
-tituloArma (_ ,16) = "Conseguiste la lanza envenenada, apartir de ahora seras conocido como Pescador en quiebra"
-tituloArma (_ ,25) = "Terminaste la partida con el 'Hacha del minotauro' ahora eres el señor del hacha, puedes ir a cortar leña"
-tituloArma (_ ,999) = "Venciste al dragón y conseguiste la espada del elegido, tu título es RandomX"
+tituloArma (_ ,1) = "¿Todavías estas con el palo de la fregona?... cobarde..."
+tituloArma (_ ,2) = "Conseguiste palo de babas al matar el slime, seras conocido como el señor de los mocos."
+tituloArma (_ ,5) = "Terminaste la partida con daga envenenada, ni tan mal, pero hay cosas mejores."
+tituloArma (_ ,8) = "Ganaste con la espada de hueso, relagalasela a tu enamorada, seguro que le encanta (jaja no, por favor no)."
+tituloArma (_ ,10) = "Terminaste la partida con el baston Descomunal, ya sabes, usalo para volver a casa."
+tituloArma (_ ,13) = "Ganaste la partida con las garras de lobo, apartir de ahora eres El gran hombre lobo pecho peludo que no usa tutú."
+tituloArma (_ ,16) = "Conseguiste la lanza envenenada, apartir de ahora seras conocido como Pescador en quiebra."
+tituloArma (_ ,25) = "Terminaste la partida con el 'Hacha del minotauro' ahora eres el señor del hacha, puedes ir a cortar leña."
+tituloArma (_ ,999) = "Venciste al dragón y conseguiste la espada del elegido, tu título es RandomX."
 tituloArma _ = ""
 
-
-
-
+-- Orden superior
+retoArmas :: Objeto -> [Monstruo] -> [Objeto]
+retoArmas a ms = filter (/=a) (map obtenerRecompensaMonstruo listaMonstruos)
 
 -- Hacemos que la dificultad del monstruo dependa de la posición considerando el numero x+y y el tamaño de la lista de mosntruos.
 enemigoDigno :: Partida -> [Monstruo] -> Monstruo
 enemigoDigno p@(m,a) ms = ms !! ((ubicacion * (length ms) `div` ((numColumnas m )+ (numColumnas m)))-1)
     where ubicacion = (fst (obtenerPosicionAventurero a)) + (fst (obtenerPosicionAventurero a))
 
+
+--                      Núcleo del juego:
 -- Nuevo juego (con opcion a carga de partida por implementar)
 nuevoJuego :: IO ()
 nuevoJuego = do
@@ -120,8 +125,10 @@ juego partida@(mazmorra, aventurero) = do
                 --Lucha
                 let aventureroHerido = lucha aventurero enemigo
                 if (obtenerVidaAventurero aventureroHerido) <= 0 then do
-                    putStrLn "Estas muerto. FIN DEL JUEGO."
+                    putStrLn "\n\tEstas muerto. FIN DEL JUEGO."
                     putStrLn (titulo (salasResueltas partida))
+                    putStrLn "\n\tEstas son otras armas que podrías haber conseguido: "
+                    putStrLn (show (retoArmas (obtenerMochilaAventurero aventurero) listaMonstruos))
                     nuevoJuego
                 else do 
                     let recompensado = obtieneObjeto aventureroHerido (obtenerRecompensaMonstruo enemigo)
@@ -131,12 +138,12 @@ juego partida@(mazmorra, aventurero) = do
                     if not (finalizado postEncuentroExito) then do
                         seleccionDestino postEncuentroExito
                         else do
-                            putStrLn "Enhorabuena has ganado. FIN DEL JUEGO."
+                            putStrLn "\n\tEnhorabuena has ganado. FIN DEL JUEGO."
                             putStrLn (titulo (salasResueltas postEncuentroExito))
                             putStrLn (tituloArma (obtenerMochilaAventurero recompensado))
+                            putStrLn "\n\tEstas son otras armas que podrías haber conseguido: "
+                            putStrLn (show (retoArmas (obtenerMochilaAventurero aventurero) listaMonstruos))
                             nuevoJuego
-                            
-
             else do 
         -- postEscuentro -->estado de la partidadespuésdelencuentro sins lucha, se modifica el estado de la sala cambia a 0
                 let postEncuentroHuida = resuelveSala partida
@@ -146,9 +153,11 @@ juego partida@(mazmorra, aventurero) = do
                 if not (finalizado postEncuentroHuida) then do
                     seleccionDestino postEncuentroHuida
                     else do
-                        putStrLn "Enhorabuena has ganado. FIN DEL JUEGO."
+                        putStrLn "\n\tEnhorabuena has ganado. FIN DEL JUEGO."
                         putStrLn (titulo (salasResueltas postEncuentroHuida))
                         putStrLn (tituloArma (obtenerMochilaAventurero aventurero))
+                        putStrLn "\n\tEstas son otras armas que podrías haber conseguido: "
+                        putStrLn (show (retoArmas (obtenerMochilaAventurero aventurero) listaMonstruos))
                         nuevoJuego
 
 -- Consulta la direccion del movimiento y llama al siguiente turno
